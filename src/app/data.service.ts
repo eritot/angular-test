58627028687c11f4613263a9d4b2670fdeda6149
eritot <erik.toth@dafgard.se>
2022-11-12 01:00:08 +0100

    call api

---
+++ src/app/data.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getMessage(): Observable<object> {
+    return this.httpClient.get(environment.angularApi + '/api/test')
+  }
+
+}
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: DataService) { }
 
   ngOnInit(): void {
+  }
+
+  showMessage() {
+    this.api.getMessage().subscribe({
+      next(value) {
+        console.log(value);
+      },
+      error(err) {
+        console.log(err);
+      },
+    })
   }
 
 }
